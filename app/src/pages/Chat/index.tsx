52df12f15931f15c81b45a1bbcc99bb144812299
Myltiane <myltiane.aux@gmail.com>
2023-05-21 21:37:13 -0300

    feat: iniciando o chat

--- app/src/pages/Chat/index.tsx
+++ app/src/pages/Chat/index.tsx
@@ -4,12 +4,14 @@ import Head from "../../components/Head";
 import ChatContent from "../../components/ChatContent";
 import { useState } from "react";
 import { User } from "../../interfaces/user";
+import { useNavigate } from "react-router";
 
 const Chat = () => {
     const [contacts, setContacts] = useState<User[]>([])
     const [currentUser, setCurrentUser] = useState<User>()
     const [currentChat, setCurrentChat] = useState(undefined)
-
+    const navigate = useNavigate();
+    
     const handleChatChange = (chat: any) => {
         setCurrentChat(chat);
     }
